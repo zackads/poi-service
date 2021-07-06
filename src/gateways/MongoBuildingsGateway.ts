a7d984d91122f263a45fdfb34d6da0ef97def526
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-06 13:56:23 +0100

    Define local MongoBuilding interface

--- src/gateways/MongoBuildingsGateway.ts
+++ src/gateways/MongoBuildingsGateway.ts
@@ -1,84 +1,88 @@
 import * as MongoClient from "mongodb";
-import {appConfig} from "../../appConfig";
-import {Building} from "../domain/Building";
-import {Polygon} from "../domain/Polygon";
-import {BuildingsGateway} from "./BuildingsGateway";
+import { appConfig } from "../../appConfig";
+import { Building } from "../domain/Building";
+import { Polygon } from "../domain/Polygon";
+import { BuildingsGateway } from "./BuildingsGateway";
+
+interface MongoBuilding {
+  _id: any;
+  properties: {
+    Name: any;
+    ListEntry: any;
+    Location: any;
+    Grade: any;
+    Hyperlink: any;
+  };
+  geometry: { type: any; coordinates: number[] };
+}
 
 export class MongoBuildingsGateway implements BuildingsGateway {
-    private config = {
-        databaseUri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`,
-        databaseName: "poi",
-        collectionName: "buildings"
-    }
-    private dbInstance;
+  private config = {
+    databaseUri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`,
+    databaseName: "poi",
+    collectionName: "buildings",
+  };
+  private dbInstance: MongoClient.Db | undefined;
 
-    public getBuildingsInPolygon(polygon: Polygon): Building[] {
-        return this.connect()
-            .then((db) => this.query(db, polygon))
-            .then((result) => {
-                return result;
-            }).catch((error) => {
-                console.log("=> an error occurred: ", error);
-                throw error;
-            });
-    }
+  public getBuildingsInPolygon(polygon: Polygon): Promise<Building[]> {
+    return this.connect()
+      .then((db) => this.query(db, polygon))
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        console.log("=> an error occurred: ", error);
+        throw error;
+      });
+  }
 
-    private connect() {
-        if (this.dbInstance) return Promise.resolve(this.dbInstance);
+  private static transform(mongoBuilding: MongoBuilding): Building {
+    return {
+      id: mongoBuilding._id,
+      properties: {
+        name: mongoBuilding.properties.Name,
+        listEntry: mongoBuilding.properties.ListEntry,
+        location: mongoBuilding.properties.Location,
+        grade: mongoBuilding.properties.Grade,
+        hyperlink: mongoBuilding.properties.Hyperlink,
+      },
+      geometry: {
+        type: mongoBuilding.geometry.type,
+        coordinates: [
+          mongoBuilding.geometry.coordinates[1],
+          mongoBuilding.geometry.coordinates[0],
+        ],
+      },
+    };
+  }
 
-        return MongoClient.connect(this.config.databaseUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-            .then((client) => {
-                this.dbInstance = client.db(this.config.databaseName);
-                return this.dbInstance;
-            })
-            .catch((error) => {
-                console.log("=> an error occurred: ", error);
-                throw error;
-            });
-    }
+  private connect() {
+    if (this.dbInstance) return Promise.resolve(this.dbInstance);
 
-    private query(db, polygon) {
-        return db
-            .collection(this.config.collectionName)
-            .find({
-                geometry: {
-                    $geoWithin: {
-                        $geometry: {
-                            type: "Polygon",
-                            coordinates: polygon,
-                        },
-                    },
-                },
-            })
-            .limit(appConfig.maxQueryRecords)
-            .toArray()
-            .then((buildings) => buildings.map(MongoBuildingsGateway.transform)
-                .catch((error) => {
-                    console.log("=> an error occurred: ", error);
-                    return { statusCode: 500, body: "error" };
-                }));
-    }
+    return MongoClient.connect(this.config.databaseUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((client) => {
+      this.dbInstance = client.db(this.config.databaseName);
+      return this.dbInstance;
+    });
+  }
 
-    private static transform(dbBuilding): Building {
-        return {
-            id: dbBuilding._id,
-            properties: {
-                name: dbBuilding.properties.Name,
-                listEntry: dbBuilding.properties.ListEntry,
-                location: dbBuilding.properties.Location,
-                grade: dbBuilding.properties.Grade,
-                hyperlink: dbBuilding.properties.Hyperlink,
+  private query(db: MongoClient.Db, polygon: Polygon) {
+    return db
+      .collection(this.config.collectionName)
+      .find({
+        geometry: {
+          $geoWithin: {
+            $geometry: {
+              type: "Polygon",
+              coordinates: [polygon],
             },
-            geometry: {
-                type: dbBuilding.geometry.type,
-                coordinates: [
-                    dbBuilding.geometry.coordinates[1],
-                    dbBuilding.geometry.coordinates[0],
-                ],
-            },
-        };
-    }
+          },
+        },
+      })
+      .limit(appConfig.maxQueryRecords)
+      .toArray()
+      .then((buildings) => buildings.map(MongoBuildingsGateway.transform));
+  }
 }
